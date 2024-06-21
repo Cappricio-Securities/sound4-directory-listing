08e2222dd42a0679995b41de3d692fa3f788592d
Karthikeyan C <114332111+cyber-karthi@users.noreply.github.com>
2024-06-21 15:07:16 +0530

    Update const.py

--- sound4directorylisting/utils/const.py
+++ sound4directorylisting/utils/const.py
@@ -12,7 +12,7 @@
 
 
 class Data:
-    blog = 'https://blogs.cappriciosec.com/blog/137/sound4-directory-listing'
+    blog = 'https://blogs.cappriciosec.com/blog/187/sound4-directory-listing'
     api = 'https://api.cappriciosec.com/Telegram/cappriciosecbot.php'
     config_path = '~/.config/cappriciosec-tools/cappriciosec.yaml'
     payloadurl = 'https://raw.githubusercontent.com/Cappricio-Securities/PayloadAllTheThings/main/sound4-directory-listing.txt'
